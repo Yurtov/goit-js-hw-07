b6e4a4859959c31e96395d2c87f7eeb4829e3225
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-06-12 22:48:41 +0300

    result

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryList = document.querySelector(".gallery");
+const gallaryMarkup = createGallaryMarkup(galleryItems);
+
+gallaryList.insertAdjacentHTML("beforeend", gallaryMarkup);
+gallaryList.addEventListener("click", handelOpenNormalImg);
+
+function createGallaryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+				/>
+            </a>
+        </li>
+    `;
+		})
+		.join("");
+}
+
+const instance = basicLightbox.create(`<img width="1280" height="auto" src="">`, {
+	onShow: instance => {
+		window.addEventListener("keydown", handelEscClose);
+	},
+	onClose: instance => {
+		window.removeEventListener("keydown", handelEscClose);
+	},
+});
+
+function handelOpenNormalImg(e) {
+	e.preventDefault();
+
+	const isDatasetEl = e.target.dataset.source;
+	if (!isDatasetEl) {
+		return;
+	}
+	instance.element().querySelector("img").src = isDatasetEl;
+	instance.show();
+}
+
+function handelEscClose(e) {
+	if (e.code !== "Escape") return;
+	instance.close();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallaryList = document.querySelector(".gallery");
+const gallaryMarkup = createGallaryMarkup(galleryItems);
+
+gallaryList.insertAdjacentHTML("beforeend", gallaryMarkup);
+
+function createGallaryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+         <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt=""${description}"
+              />
+          </a>
+        </li>
+    `;
+		})
+		.join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
+});
